4b9e44aed3e75be83e37875ca03a1cc97da7128a
Kumar McMillan <kumar.mcmillan@gmail.com>
2016-09-18 23:16:06 -0500

    average play counts, only focus on albums

--- prune_itunes_lib.py
+++ prune_itunes_lib.py
@@ -8,16 +8,19 @@ from xml.etree import ElementTree
 from urllib import unquote
 from os.path import join as pjoin
 import pprint
+from collections import defaultdict
+from decimal import Decimal
+
 
 def search_lib(library):
     library_xml = pjoin(library, 'iTunes Music Library.xml')
     pl = ElementTree.parse(library_xml)
     dicts = pl.findall('dict/dict/dict')
-    to_purge = set()
     skipped = 0
     found = 0
 
     print 'Looking for old unplayed albums'
+    play_counts = defaultdict(list)
     for d in dicts:
         track = {}
         key = 'Unknown'
@@ -33,31 +36,43 @@ def search_lib(library):
         #pprint.pprint(track)
 
         try:
-            if int(track['Play Count']) <= 1:
-                title = u''
-                if not track.get('Compilation'):
-                    title = u'{} - '.format(track['Artist'])
-                title = u'{}{}'.format(title, track['Album'])
-                to_purge.add((track['Date Added'][0:10], title))
-                found += 1
+            title = u''
+            if not track.get('Compilation'):
+                title = u'{} - '.format(track['Artist'])
+            title = u'{}{}'.format(title, track['Album'])
+            play_counts[(track['Date Added'][0:10], title)].append(
+                int(track['Play Count'] or 0))
+            found += 1
         except KeyError:
             skipped += 1
 
     print 'Found: {}'.format(found)
     print 'Skipped: {}'.format(skipped)
 
+    print 'Checking for rarely played albums'
+    to_purge = []
+    for key, counts in play_counts.iteritems():
+        # Skip anything with less than 5 tracks because it's
+        # not worth the time to delete these.
+        if len(counts) < 5:
+            continue
+        # Purge anything that was only played once (on average)
+        if (Decimal(sum(counts)) / Decimal(len(counts))) <= Decimal(1):
+            to_purge.append(key)
+
     for year_added, release in sorted(to_purge):
-        line = u'{} ({})'.format(release, year_added)
+        line = u'{} (Added on {})'.format(release, year_added)
         print line.encode('utf8')
 
+
 def main():
-    '''extract song files from an iTunes XML playlist'''
+    """Print a list of rarely played iTunes music, oldest first"""
     default_library = pjoin('/Users', os.environ['USER'], 'Music', 'iTunes')
 
     parser = optparse.OptionParser(usage='%prog' + "\n" + main.__doc__)
     parser.add_option('-l','--library', default=default_library,
-                        help=   'iTunes music library directory.  '
-                                'defaults to %s' % default_library)
+                      help=   'iTunes music library directory.  '
+                              'defaults to %s' % default_library)
     (options,args) = parser.parse_args()
 
     library = options.library
